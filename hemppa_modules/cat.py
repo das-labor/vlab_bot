816bc91c087ec95ca3a58f6c0343c531eaa0a529
Marco Bakera <marco@bakera.de>
2021-03-28 14:32:10 +0200

    fix: cat image will be shown

--- hemppa_modules/cat.py
+++ hemppa_modules/cat.py
@@ -11,7 +11,19 @@ class MatrixModule(BotModule):
             js = json.load(response)
 
         if 'url' in js:
-            await bot.send_text(room, f'{HOST}/{js["url"]}')
-        
+            img_url = HOST + js["url"]
+            matrix_img_url = await bot.upload_image(img_url)
+            if matrix_img_url:
+                await bot.send_image(room, matrix_img_url, "a cat")
+
+            # Old version just linking to the image. Will not be shown by 
+            # default.
+            '''
+            await bot.send_html(
+                room,
+                f"cat <img src='{img_url}'> content",
+                f"Cat content {img_url}")
+            '''
+
     def help(self):
         return "Cat content."
