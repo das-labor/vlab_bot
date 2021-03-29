620fea1ec51943d6a5de63124c2e999c080f42a1
Marco Bakera <marco@bakera.de>
2021-03-29 18:10:24 +0200

    cal: inform about events

--- hemppa_modules/cal.py
+++ hemppa_modules/cal.py
@@ -3,19 +3,44 @@ from urllib.request import urlopen
 import datetime
 import json
 import xml.etree.ElementTree as ET
+import os
 
 CAL_RSS_URL ="https://www.das-labor.org/termine.rss"
-MAX_NUMBER_EVENTS = 3
+MAIN_ROOM_ID = os.environ["VLAB_MAIN_ROOM_ID"]
 
 class MatrixModule(BotModule):
+    def __init__(self, name):
+        super().__init__(name)
+        self.poll_interval = 6 * 10 # * 10 seconds
+        self.main_room = MAIN_ROOM_ID
+
     async def matrix_message(self, bot, room, event):
         msg = '📅 Termine der nächsten Tage:\n'
 
         for date, title, link in self.next_events():
-            msg += f'{date} {title}\n {link}\n'
+            evdat = date.strftime('%Y-%m-%d %H:%M')
+            msg += f'{evdat} {title}\n {link}\n'
 
         msg += 'https://wiki.das-labor.org/w/Kalender'
         await bot.send_text(room, msg)
+
+    async def matrix_poll(self, bot, pollcount):
+        'called every 10 seconds'
+        if pollcount % self.poll_interval != 0:
+            return
+
+        event_date, title, link = self.next_events(num=1)[0]
+        # now  |  event date | next poll interval
+        #
+        time_until_event = event_date - datetime.datetime.now()
+        if time_until_event.total_seconds() < self.poll_interval * 10:
+            room = bot.get_room_by_id(self.main_room)
+            if room is None:
+                return
+
+            msg = f"Gleich: {title} ({event_date})\n{link}"
+            self.logger.debug(f'notify event: {msg}')
+            await bot.send_text(room, msg)
 
     def next_events(self, num=3):
         'return the next num events (date, title, link).'
@@ -28,10 +53,9 @@ class MatrixModule(BotModule):
             it_link = item.find('link').text
 
             event_date, title = self.extract(it_desc)
-            evdat = event_date.strftime('%Y-%m-%d %H:%M')
-            events.append( (evdat, title, it_link))
+            events.append( (event_date, title, it_link))
 
-            if len(events) >= MAX_NUMBER_EVENTS:
+            if len(events) >= num:
                 return events
 
     def help(self):
