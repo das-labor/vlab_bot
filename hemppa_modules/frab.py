ace4064de3aa32d0950fac98ade1b1553e1dcd9b
Marco Bakera <marco@bakera.de>
2021-03-29 14:26:47 +0200

    doc frab: info about commands

--- hemppa_modules/frab.py
+++ hemppa_modules/frab.py
@@ -141,5 +141,5 @@ class MatrixModule(BotModule):
             await self.check_events(bot, room, url)
 
     def help(self):
-        return "Ankündigung von Events aus dem Fahrplan"
+        return "Ankündigung von Events aus dem Fahrplan (add, rm, ls)"
 
