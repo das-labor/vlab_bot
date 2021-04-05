36c7a5675d1cb72a8c9e0103cf25d8b0c1e3734a
Marco Bakera <marco@bakera.de>
2021-04-05 14:26:40 +0200

    wiki: information about changes in wiki

--- hemppa_modules/wiki.py
+++ hemppa_modules/wiki.py
@@ -1,16 +1,22 @@
-# Bot module for https://github.com/vranki/hemppa
-
+import xml.etree.ElementTree as ET
 from modules.common.module import BotModule
 from urllib.request import urlopen
 import urllib.parse
 import json
+import os
+import datetime
 
-# https://www.mediawiki.org/wiki/API:Search
 NUM_RESULTS = 3
 WIKI_BASE_URL = "https://wiki.das-labor.org"
 API_SEARCH_PREFIX = WIKI_BASE_URL + f"/api.php?action=query&format=json&list=search&srlimit={NUM_RESULTS}&srsearch="
+MAIN_ROOM_ID = os.environ["VLAB_BOT_MAIN_ROOM_ID"]
+RECENT_CHANGES_URL = WIKI_BASE_URL + '/api.php?action=feedrecentchanges&hidebots=1&hideminor=1&days=1&limit=10&namespace=0&feedformat=atom'
 
 class MatrixModule(BotModule):
+    def __init__(self,name):
+        super().__init__(name)
+        self.poll_interval = 6*60*24 # * 10 seconds
+
     async def matrix_message(self, bot, room, event):
         # !wiki some query
         args = event.body.split()
@@ -36,6 +42,34 @@ class MatrixModule(BotModule):
                     f'{WIKI_BASE_URL}?search=' + query
 
             await bot.send_text(room, answer)
-        
+
+    async def matrix_poll(self, bot, pollcount):
+        'called every 10 seconds'
+        if pollcount % self.poll_interval != 0:
+            return
+
+        self.logger.debug('polling recent changes')
+
+        room = bot.get_room_by_id(self.mainroom_id)
+        if room is None:
+            return
+
+        msg = '🔎 Im Wiki gab es ein paar Änderungen\n'
+        root = ET.parse(urlopen(RECENT_CHANGES_URL, timeout=5)).getroot()
+
+        ns = '{http://www.w3.org/2005/Atom}'
+        num_entries = 0
+        for entry in root.iter(f'{ns}entry'):
+            href = entry.find(f'{ns}link').attrib['href']
+            title = entry.find(f'{ns}title').text
+            msg += f'- {title}: {href}\n'
+            num_entries += 1
+
+        if num_entries > 0:
+            self.logger.debug('posting recent changes')
+            await bot.send_text(room, msg)
+
     def help(self):
-        return "🔎 Durchsuche das Labor-Wiki."
+        update_hours = int((self.poll_interval * 10) / 60 / 60)
+        return "🔎 Durchsuche das Labor-Wiki. Berichte alle " + \
+            f"{update_hours} Stunden über Änderungen am Wiki."
