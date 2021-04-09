2c6e09cf794ebb21b23323a69223b19d0f2b2ceb
Mythozz <mythozz@das-labor.org>
2021-04-09 23:17:57 +0200

    Add Tschunk recipe

---
+++ hemppa_modules/tschunk.py
@@ -0,0 +1,18 @@
+from modules.common.module import BotModule
+
+
+class MatrixModule(BotModule):
+
+    async def matrix_message(self, bot, room, event):
+        msg = f'1/2 Limette\n
+                2EL (20g) Brauner Zucker\n
+                100-200g Crushed Ice\n
+                4cl Rum\n
+                100-150ml Mate\n
+                1 Strohhalm\n
+                (bei Entropia geklaut)\n\n'
+        
+        await bot.send_text(room, msg)
+
+    def help(self):
+        return '📝 Tschunkrezept.'
