c7817382e9fd7e79541f20a56f7648dd0074affc
Marco Bakera <marco@bakera.de>
2021-04-10 08:54:07 +0200

    tschunk: fix line endings

--- hemppa_modules/tschunk.py
+++ hemppa_modules/tschunk.py
@@ -4,13 +4,13 @@ from modules.common.module import BotModule
 class MatrixModule(BotModule):
 
     async def matrix_message(self, bot, room, event):
-        msg = f'1/2 Limette\n
-                2EL (20g) Brauner Zucker\n
-                100-200g Crushed Ice\n
-                4cl Rum\n
-                100-150ml Mate\n
-                1 Strohhalm\n
-                (bei Entropia geklaut)\n\n'
+        msg = '1/2 Limette\n' + \
+               '2EL (20g) Brauner Zucker\n' + \
+                '100-200g Crushed Ice\n' + \
+                '4cl Rum\n' + \
+                '100-150ml Mate\n' + \
+                '1 Strohhalm\n' + \
+                '(bei Entropia geklaut)'
         
         await bot.send_text(room, msg)
 
