0b769bf18af9c6b31a7ef9917e08dc37dedbbc4e
Marco Bakera <marco@bakera.de>
2021-04-10 08:59:23 +0200

    tschunk: indentation

--- hemppa_modules/tschunk.py
+++ hemppa_modules/tschunk.py
@@ -6,11 +6,11 @@ class MatrixModule(BotModule):
     async def matrix_message(self, bot, room, event):
         msg = '1/2 Limette\n' + \
                '2EL (20g) Brauner Zucker\n' + \
-                '100-200g Crushed Ice\n' + \
-                '4cl Rum\n' + \
-                '100-150ml Mate\n' + \
-                '1 Strohhalm\n' + \
-                '(bei Entropia geklaut)'
+               '100-200g Crushed Ice\n' + \
+               '4cl Rum\n' + \
+               '100-150ml Mate\n' + \
+               '1 Strohhalm\n' + \
+               '(bei Entropia geklaut)'
         
         await bot.send_text(room, msg)
 
