a5e52e2ebd484362e89a9ab75c2a14388e48fd27
Mythozz <mythozz@das-labor.org>
2021-04-14 00:05:15 +0200

    BOFH module: Change quotation maks

--- hemppa_modules/bofh.py
+++ hemppa_modules/bofh.py
@@ -1,6 +1,5 @@
 from modules.common.module import BotModule
 from urllib.request import urlopen
-import json
 
 HOST = 'http://pages.cs.wisc.edu/~ballard/bofh/bofhserver.pl'
 API = HOST + ''
@@ -12,7 +11,7 @@ class MatrixModule(BotModule):
             msg_search_string = "<font size = \"+2\">"
             msg_html_start_pos = msg_html.rindex(msg_search_string) + len(msg_search_string)
             msg_html_end_pos = msg_html.index("\n</font>")
-            msg = "BOFH excuse generator: \"" + msg_html[msg_html_start_pos:msg_html_end_pos] + "\""
+            msg = 'BOFH excuse generator: ' + msg_html[msg_html_start_pos:msg_html_end_pos]
 
             await bot.send_text(room, msg)
 
