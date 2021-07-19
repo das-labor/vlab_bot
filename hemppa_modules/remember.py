e700162dcb47fcb455ad57df1adcb463648d67b4
Marco Bakera <marco@bakera.de>
2021-07-19 10:07:31 +0200

    remember: line breaks in help

--- hemppa_modules/remember.py
+++ hemppa_modules/remember.py
@@ -102,10 +102,11 @@ class MatrixModule(BotModule):
     def long_help(self, bot, event, **kwargs):
         text = self.help() + \
             ' Neue Erinnerungen können mit Angabe eines Datums ' + \
-            'hinzugefügt werden: !remember 2042-01-01T00:01 Sylvester'
+            'hinzugefügt werden:\n ' +\
+            '!remember 2042-01-01T00:01 Sylvester'
 
         if bot.is_owner(event):
-            text += '!remember ls listet aktuelle Erinnerungen auf.'
+            text += '\n!remember ls listet aktuelle Erinnerungen auf.'
 
         return text
     
