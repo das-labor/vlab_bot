811b004d3fe803f0db7c3e2c88d7e6da53e1ecf3
Marco Bakera <marco@bakera.de>
2021-07-19 21:11:24 +0200

    workadv: new module to poll work adventure instances

---
+++ hemppa_modules/workadv.py
@@ -0,0 +1,58 @@
+from modules.common.pollingservice import PollingService
+from urllib.request import urlopen
+
+NUM_CLIENTS_MARKER = 'workadventure_nb_clients_per_room'
+
+class MatrixModule(PollingService):
+    def __init__(self, name):
+        super().__init__(name)
+        self.poll_interval_min = 10
+        self.poll_interval_random = 1
+        self.accountroomid_lastnumonline = {}
+
+    async def poll_implementation(self, bot, account, roomid, send_messages):
+        self.logger.debug(f'polling workadventure {account}.')
+        room, url = account.split('@')
+        self.logger.debug(f'looking for room {room} at {url}')
+
+        with urlopen(url, timeout=5) as response:
+            lines = response.readlines()
+
+        for line in lines:
+            line_str = line.decode()
+            if NUM_CLIENTS_MARKER in line_str and room in line_str:
+                # line of format
+                # workadventure_nb_clients_per_room{room="_/global/das-labor.github.io/workadv_das-labor/main.json"} 20
+                num_clients_online = int(line_str.split('} ')[1])
+                self.logger.debug(f'found {num_clients_online} clients online')
+
+                last_num_online = self.accountroomid_lastnumonline.get(account+roomid, 0)
+                self.logger.debug(f'last num online {last_num_online}')
+
+                if send_messages and num_clients_online != last_num_online:
+                    await bot.send_text(bot.get_room_by_id(roomid), 
+                        f'{num_clients_online} entities online')
+
+                    self.accountroomid_lastnumonline[account+roomid] = num_clients_online
+                    bot.save_settings()
+                    break
+
+    def get_settings(self):
+        data = super().get_settings()
+        data['last_num_online'] = self.accountroomid_lastnumonline
+        return data
+
+    def set_settings(self, data):
+        super().set_settings(data)
+        if data.get('last_num_online'):
+            self.accountroomid_lastsent = data['last_num_online']
+
+    def help(self):
+        return "Notify about entities in a work adventure instances."
+
+    def long_help(self, bot, event, **kwargs):
+        return self.help() + \
+            ' This is a polling service. Therefore there are additional ' + \
+            'commands: list, debug, poll, clear, add URL, del URL\n' + \
+            '!wordadv add URL: to add a new endpoint. URL has the form\n' + \
+            'ROOM@METRICS_URL e.g. _/global/das-labor.github.io/workadv_das-labor/main.json@https://pusher.wa.binary-kitchen.de/metrics,'
